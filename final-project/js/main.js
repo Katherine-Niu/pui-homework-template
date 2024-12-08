e381ff6258025135f282c267b1f83e59b22f2490
Katherine-Niu <katherineniu@gmail.com>
2024-12-08 14:44:04 -0500

    added gsap animation, comments, and references

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,3 +1,6 @@
+//Creating an array to store the questions and corresponding answers for each question. 
+//Each question has 4 answers and each answer contains text, a image file, and a value.
+
 const questions = [
     {
       question: "Which element represents your personality?",
@@ -11,12 +14,9 @@ const questions = [
       question: "If your life had a soundtrack, which genre would it be?",
       possibleAnswers: [{text: "Reggae beats, relaxed and rhythmic", imageFile:"reggae.jpeg", value: "reggae"}, {text: "Classic rock anthems, bold and unapologetic", imageFile: "classic-rock.jpg", value: "rock"}, {text: "Smooth jazz, harmonic and sophisticated", imageFile: "jazz.jpeg", value: "jazz"}, {text: "Hip-hop records, poetic and expressive", imageFile: "hip-hop.jpg", value: "pop"}],
     },
-    /*{
-      question: "How do you want your fragrance to feel?",
-      possibleAnswers: [{text: "Gentle and subtle", imageFile: "gentle1.jpeg"}, {text: "Balanced and natural", imageFile: "balanced.jpeg"}, {text: "Rich and bold", imageFile: "bold.jpeg"}, {text: "Strange and fascinating", imageFile: "strange.jpeg"}],
-    },*/
   ];
 
+//Creating an array to store fragrance names and the corresponding values. The values represent a unique answer combination. 
 const fragranceNames = [
     {
         name: "Dead Air",
@@ -276,6 +276,8 @@ const fragranceNames = [
     },
 ];  
 
+
+//Creating an array to store the information for each fragrance - description, image file, and brand. 
 const fragranceDescriptions = 
   {
     "Dead Air": {description: "Woody, floral, green, Dead Air is an intriguing and mysterious fragrance. A dark fantasy of shadows and decadent drama, a breeze of davana and violet leaf is felt at the top, accompanied by a heart of narcissus that awakens the senses and stimulates the imagination. A captivating composition of date, oak wood and vetiver adds a touch of complexity.", 
@@ -328,31 +330,42 @@ const fragranceDescriptions =
       brand: "Nasomatto"},
   };
 
-
-const quizProgress = document.getElementById("progress");
-const quizContainer = document.getElementById("container")
+//Grabbing the relevant html elements
+const quizContainer = document.getElementById("container");
 const questionContainer = document.getElementById("questionContainer");
 const answerContainer = document.getElementById("answerContainer");
-const previousBtn = document.getElementById("previousBtn");
-const nextBtn = document.getElementById("nextBtn");
 const resultsContainer = document.getElementById("results");
-const fragranceResultContent = document.querySelector(".fragranceResultContent")
-const fragranceImageFrame = document.querySelector(".fragranceImageFrame")
-const fragranceText = document.querySelector(".fragranceText")
-const exploreAnswersSection = document.querySelector(".exploreAnswersSection")
-const differentAnswer = document.querySelector(".differentAnswer")
+const fragranceResultContent = document.querySelector(".fragranceResultContent");
+const fragranceImageFrame = document.querySelector(".fragranceImageFrame");
+const fragranceText = document.querySelector(".fragranceText");
+const exploreAnswersSection = document.querySelector(".exploreAnswersSection");
+const differentAnswer = document.querySelector(".differentAnswer");
+
+//setting the index for the current question
 let currentQuestionIndex = 0;
+
+//variables to store each individual user answer
 let userAnswer1 = "";
 let userAnswer2 = "";
 let userAnswer3 = "";
+
+//array to store all user answers
 let userAnswers = [];
-  
+
+//string to store the fragranceResult, which is the fragrance that the user is matched to based on their answers
+let fragranceResult = ""; 
+
+//string to store the fragranceDescription
+let fragranceDescription = "";
+
+//ths function populates the html with the question and answers based on the question index. 
 function handleQuestion(index) {
   
-    // topic/question
+    //populating the question container with the current question from the questions array
     questionContainer.innerText = questions[index].question;
 
-    // answers
+    //populating the answer container with the answers to the current question
+    //a button will be created for each answer, containing the corresponding value, image file, and text
     answerContainer.innerHTML = "";
     questions[index].possibleAnswers.forEach((answer) => {
       answerContainer.innerHTML += 
@@ -364,73 +377,101 @@ function handleQuestion(index) {
         <div class="overlay"></div>
       </button>`;
     });
+
+    //a click event listener is added to each answer
+    //when an answer is clicked, the value will be added to the array that stores the user's answers and variables representing each user answer will be updated
+    //when an answer is clicked, the animateAnswers function will be called to animate the buttons
     let answers = document.querySelectorAll(".quizAnswer");
     answers.forEach((answer) => {
       answer.addEventListener("click", (e) => {
-        if (currentQuestionIndex === questions.length-1) {
-        setTimeout(endQuiz, 3000);
-        } else {
-        currentQuestionIndex++;
-        }
-        handleQuestion(currentQuestionIndex);
-        userAnswers.push(answer.value);
+        let answerValue = answer.value;
+        userAnswers.push(answerValue);
         userAnswer1 = userAnswers[0];
         userAnswer2 = userAnswers[1];
         userAnswer3 = userAnswers[2];
-        console.log(userAnswers);
-        console.log("user answer 1: " + userAnswer1)
-        console.log("user answer 2: " + userAnswer2)
-        console.log("user answer 3: " + userAnswer3)
-        //calculateResult()
+        animateAnswers();
       });
     });
   };
 
-let fragranceResult = "";  
-  
+//this function uses the GSAP javascript library to apply a spinning and fading animation for each button 
+//after the animation is complete, the tlComplete function is called which increases the question index and handles the next question or ends the quiz
+function animateAnswers() {
+  gsap.to(".quizAnswer", {
+    duration: 1,
+    rotation: 360,
+    opacity: 0, 
+    delay: 0.5, 
+    stagger: 0.2,
+    ease: "sine.out", 
+    force3D: true,
+    onComplete: tlComplete
+  });
+};
+
+//once all of the questions are answered, the endQuiz function will be called
+//otherwise, the current question index will increase by 1 and the next question will be populated
+function tlComplete(){
+  if (currentQuestionIndex === questions.length-1) {
+    endQuiz();
+    } else {
+    currentQuestionIndex++;
+    }
+  handleQuestion(currentQuestionIndex);
+};
+
+
+//this function stores the userAnswers array as a string and calculates the matching fragrance
+//it loops through the fragranceNames array
+//if theres's a match between the fragranceNames[i].values and userAnswersString, the fragranceResult will be set to fragranceNames[i].name
 function calculateResult() {
     userAnswersString = JSON.stringify(userAnswers)
-    console.log(userAnswersString)
+    console.log(userAnswersString);
     for (let i=0; i<fragranceNames.length; i++) {
         if (fragranceNames[i].values === userAnswersString) {
             fragranceResult = fragranceNames[i].name;
             return fragranceResult;
-
         }
     }
+};
 
-}
 
-let fragranceDescription = "";
-
+//this function calculcates the matching description based on the fragranceResult
 function calculateDescription() {
-  fragranceDescription = fragranceDescriptions[fragranceResult].description
+  fragranceDescription = fragranceDescriptions[fragranceResult].description;
   return fragranceDescription;
-}
+};
 
-/*
-function calculateDescription() {
-  for (let i=0; i<fragrancesAll.length; i++) {
-    if (fragrancesAll[i].values === userAnswersString) {
-      fragranceDescription = fragrancesAll[i].description
-      console.log("Fragrance description " + fragranceDescription)
-      return fragranceDescription;
-    }
-  }
-}
-*/
 
+//this function calculates the matching image file based on the fragranceResult
 function calculateImageFile() {
-  fragranceImageFile = fragranceDescriptions[fragranceResult].imageFile
+  fragranceImageFile = fragranceDescriptions[fragranceResult].imageFile;
   return fragranceImageFile;
 };
 
+//this function calculates the matching brand based on the fragranceResult
 function calculateBrand() {
-  fragranceBrand = fragranceDescriptions[fragranceResult].brand
+  fragranceBrand = fragranceDescriptions[fragranceResult].brand;
   return fragranceBrand;
 };
 
+//this function is called when all of the questions are answered
+//the quizContainer is hidden and the resultsContainer is made visible
+//functions are called to calculate the matching fragrance and the description, image file, and brand for that fragrance
+//the HTML for the resultsContainer is populated with all of the content for the matching fragrance
+function endQuiz(){
+  quizContainer.classList.add('hide')
+  resultsContainer.classList.remove('hide');
+  calculateResult();
+  calculateDescription();
+  calculateImageFile();
+  calculateBrand();
+  populateResultContent(); 
+};
 
+
+//this function populates the resultsContainer with the content for the matching fragrance - image file, brand, description
+//it also displays the answers that the user chose and allows them to select an alternative answer to see how that would change their result
 function populateResultContent() {
   resultsContainer.innerHTML = 
   `<h2>Quiz Completed!</h2>
@@ -466,10 +507,10 @@ function populateResultContent() {
               <div class="alternativeAnswers">
                 <p>Try a Different Answer</p>
                 <div>
-                  <button class="differentAnswer" value="water" onclick="updateResults(event)">Water</button>
-                  <button class="differentAnswer" value="earth" onclick="updateResults(event)">Earth</button>
-                  <button class="differentAnswer" value="fire" onclick="updateResults(event)">Fire</button>
-                  <button class="differentAnswer" value="air" onclick="updateResults(event)">Air</button>
+                  <button class="differentAnswer" value="water" onclick="updateResults(event)">water</button>
+                  <button class="differentAnswer" value="earth" onclick="updateResults(event)">earth</button>
+                  <button class="differentAnswer" value="fire" onclick="updateResults(event)">fire</button>
+                  <button class="differentAnswer" value="air" onclick="updateResults(event)">air</button>
                 </div>
               </div>
             </div>
@@ -484,10 +525,10 @@ function populateResultContent() {
               <div class="alternativeAnswers">
                 <p>Try a Different Answer</p>
                 <div>
-                  <button class="differentAnswer" value="garden" onclick="updateResults(event)">Garden</button>
-                  <button class="differentAnswer" value="marketplace" onclick="updateResults(event)">Marketplace</button>
-                  <button class="differentAnswer" value="forest" onclick="updateResults(event)">Forest</button>
-                  <button class="differentAnswer" value="jungle" onclick="updateResults(event)">Jungle</button>
+                  <button class="differentAnswer" value="garden" onclick="updateResults(event)">garden</button>
+                  <button class="differentAnswer" value="marketplace" onclick="updateResults(event)">marketplace</button>
+                  <button class="differentAnswer" value="forest" onclick="updateResults(event)">forest</button>
+                  <button class="differentAnswer" value="jungle" onclick="updateResults(event)">jungle</button>
                 </div>
               </div>
             </div>
@@ -502,10 +543,10 @@ function populateResultContent() {
               <div class="alternativeAnswers">
                 <p>Try a Different Answer</p>
                 <div>
-                  <button class="differentAnswer" value="reggae" onclick="updateResults(event)">Reggae</button>
-                  <button class="differentAnswer" value="rock" onclick="updateResults(event)">Rock</button>
-                  <button class="differentAnswer" value="jazz" onclick="updateResults(event)">Jazz</button>
-                  <button class="differentAnswer" value="pop" onclick="updateResults(event)">Pop</button>
+                  <button class="differentAnswer" value="reggae" onclick="updateResults(event)">reggae</button>
+                  <button class="differentAnswer" value="rock" onclick="updateResults(event)">rock</button>
+                  <button class="differentAnswer" value="jazz" onclick="updateResults(event)">jazz</button>
+                  <button class="differentAnswer" value="pop" onclick="updateResults(event)">pop</button>
                 </div>
               </div>
             </div>
@@ -515,16 +556,23 @@ function populateResultContent() {
     <button onclick="restartQuiz()">Restart Quiz</button>`;
 };
 
+//this function updates the resultsContainer based on the alternative answer that the user selects
+//it first updates the userAnswers array with the new answer
+//next it calculates the new fragrance result and the corresponding description, image file, and brand
+//finally it populates the resultsContainer with the new content
 function updateResults(event) {
   let buttonValue=event.target.value
   if (buttonValue === "earth" || buttonValue === "water" || buttonValue === "fire" || buttonValue === "air") {
     userAnswers[0]= buttonValue.toString()
+    userAnswer1=buttonValue.toString()
   }
   else if (buttonValue === "garden" || buttonValue === "marketplace" || buttonValue === "jungle" || buttonValue === "forest") {
     userAnswers[1]= buttonValue.toString()
+    userAnswer2=buttonValue.toString()
   }
-  else if (buttonValue.toString === "rock" || buttonValue.toString === "jazz" || buttonValue.toString === "pop" || buttonValue.toString === "reggae"){
+  else if (buttonValue === "rock" || buttonValue === "jazz" || buttonValue === "pop" || buttonValue === "reggae"){
     userAnswers[2]=buttonValue.toString()
+    userAnswer3=buttonValue.toString()
   }
   console.log(buttonValue)
   console.log(userAnswers)
@@ -535,81 +583,8 @@ function updateResults(event) {
   populateResultContent();
 };
 
-/*
-  fragranceImageFrame.innerHTML = `
-    <img src="../final-project/fragrance-images/${fragranceImageFile}">`;
-
-    fragranceText.innerHTML = `
-    <p>${fragranceBrand}</p>
-    <p>${fragranceResult}</p>
-    <p>${fragranceDescription}</p>
-    <div class=fragranceDetails>
-        <div>
-          <p>Fragrance Family</p>
-          <p>Spicy</p>
-        </div>
-        <div>
-          <p>Notes</p>
-          <p>Pink peppercorn, Juniper, Orange Ring Pop, 
-            Night Blooming Jasmine, Wormwood, Sandalwood, 
-            Cedarwood, Smoke, Patchouli, Rum.</p>
-        </div>
-    </div>`;
-
-    exploreAnswersSection.innerHTML = `
-    <div class="exploreQuestion">
-        <p>Question 1</p>
-        <div class="yourAnswer">
-            <p>Your Answer</p>
-            <p>${userAnswer1}</p>
-        </div>
-        <div class="alternativeAnswers">
-            <p>Try a Different Answer</p>
-            <div>
-                <p>Earth</p>
-                <p>Fire</p>
-                <p>Water</p>
-                <p>Air</p>
-            </div>
-        </div>
-    </div>`;
-};
-*/
-
-function endQuiz(){
-    quizContainer.classList.add('hide')
-    resultsContainer.classList.remove('hide');
-    
-    calculateResult();
-    calculateDescription();
-    calculateImageFile();
-    calculateBrand();
-    /*
-    const nameElement = document.createElement('p');
-    nameElement.textContent = fragranceResult;
-    fragranceTextElement.appendChild(nameElement);
-    
-    
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = "hi";
-    */
-    populateResultContent();
-    
-/*
-    fragranceText.innerHTML = `
-    <h2>Quiz Completed!</h2>
-    <div class="fragranceResultContent">
-      <div class="fragranceText">
-        <p>Your result is ${fragranceResult}</p>
-        <p>Description is ${fragranceDescription}</p>
-      </div>
-      <img class="fragranceImage" src="../final-project/page-1-images/${fragranceImageFile}">
-    </div>
-    <button onclick="restartQuiz()">Restart Quiz</button>`;
-*/
-    
-};
-
+//this function restarts the quiz
+//it hides the resultsContainer, reveals the quizContainer, clears the userAnswers array, sets the currentQuestionIndex to 0, and populates the HTML with the first quiz question
 function restartQuiz() {
     resultsContainer.classList.add('hide');
     resultsContainer.innerHTML = "";
@@ -619,4 +594,25 @@ function restartQuiz() {
     handleQuestion(currentQuestionIndex);
 };
 
+//calling the handleQuestion function to initiate the quiz
 handleQuestion(currentQuestionIndex);
+
+/*
+References
+
+I pulled handleQuestion function from this tutorial and adapted it to my website.
+https://www.youtube.com/watch?v=_FnFPmA87XU
+https://github.com/treehouse/quiz-app
+
+I learned how to hide and show HTML content using classList.add and classList.remove
+https://hackr.io/blog/how-to-build-a-javascript-quiz-app
+
+I pulled the code for the spinning animation from this GSAP tutorial
+https://codepen.io/GreenSock/pen/LYdzaoz 
+
+I learned how to use a callback function to handle the next quiz question after the animation ends
+https://gsap.com/resources/getting-started/control/ 
+
+I pulled fragrance images and descriptions from Nose Paris
+https://noseparis.com/en/
+*/
